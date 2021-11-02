321e2359e19e3336bc3c5ce128791c3dc91c9144
romanshliakhov <romanshliakhov@gmail.com>
2021-11-03 00:50:35 +0200

    starting modals

--- source/js/common.js
+++ source/js/common.js
@@ -1,53 +1,167 @@
-// alert
-let playWithoutLimits = document.querySelector('.games__item-status--demo');
+// modals
+class Modal {
+  constructor (options) {
+      let defaultOptions = {
+          isOpen: () => {},
+          isClose: () => {},
+      };
+      this.options = Object.assign(defaultOptions, options);
+      this.modal = document.querySelector('.modal');
+      this.speed = false;
+      this.animation = false;
+      this.isOpen = false;
+      this.modalContainer = false;
+      this.previousActiveElement = false;
+      this.fixBlocks = document.querySelectorAll('.fix-block');
+      this.focusElements = [
+          'a[href]',
+          'input',
+          'button',
+          'select',
+          'textarea',
+          '[tabindex]'
+      ];
+      this.events();
+  }
 
-playWithoutLimits.onclick = function () {
-  playWithoutLimits.classList.toggle('games__item-status--reg');
-};
+  events() {
+      if (this.modal) {
+          document.addEventListener('click', function (e) {
+              const clickedElement = e.target.closest('[data-path]');
+              if (clickedElement) {
+                  let target = clickedElement.dataset.path;
+                  let animation = clickedElement.dataset.animation;
+                  let speed = clickedElement.dataset.speed;
+                  this.animation = animation ? animation : 'fade';
+                  this.speed = speed ? parseInt(speed) : 300;
+                  this.modalContainer = document.querySelector(`[data-target="${target}"]`);
+                  this.open();
+                  return;
+              }
 
-// modal
-$('[data-modal=registration]').on('click', function () {
-  $('.overlay, #registration').fadeIn();
-});
+              if (e.target.closest('.modal-close')) {
+                  this.close();
+                  return;
+              }
+          }.bind(this));
 
-$('.modal__close').on('click', function () {
-  $('.overlay, #registration').fadeOut();
-});
+          window.addEventListener('keydown', function (e) {
+              if (e.keyCode == 27) {
+                  if (this.isOpen) {
+                      this.close();
+                  }
+              }
+          }.bind(this));
 
+          this.modal.addEventListener('click', function (e) {
+              if (!e.target.classList.contains('.modal__container') && !e.target.closest('.modal__container') && this.isOpen) {
+              this.close();
+              }
+          }.bind(this));
+      }
+  }
 
-// modal
-// (document.getElementById("popup").onclick = function () {
-//   document.getElementById("overlay").classList.toggle("overlay--active"),
-//     document.getElementById("modal").classList.toggle("modal--active");
-// }),
-// (document.getElementById("modal-close").onclick = function () {
-//   document.getElementById("overlay").classList.remove("overlay--active"),
-//     document.getElementById("modal").classList.remove("modal--active");
-// }),
-// document.getElementById("overlay").addEventListener("click", function (e) {
-//   "overlay" == e.target.id &&
-//     (this.classList.remove("overlay--active"),
-//       document.getElementById("modal").classList.remove("modal--active"));
-// });
+  open() {
+      this.previousActiveElement = document.activeElement;
 
-// popup
-const popupLinks = document.querySelectorAll(".popup-link");
-const body = document.querySelector("body");
-const lockPadding = document.querySelectorAll(".lock-padding");
+      this.modal.style.setProperty('--transition-time', `${this.speed / 1000}s`);
+      this.modal.classList.add('is-open');
+      this.disableScroll();
 
-let unlock = true;
+      this.modalContainer.classList.add('modal-open');
+      this.modalContainer.classList.add(this.animation);
 
-const timeOut = 800;
+      setTimeout(() => {
+          this.modalContainer.classList.add('animate-open');
+          this.options.isOpen(this);
+          this.isOpen = true;
+          this.focusTrap();
+      }, this.speed);
+  }
 
-if (popupLinks.length > 0) {
-  for (let index = 0; index < popupLinks.length; index++) {
-    const popupLink = popupLinks[index];
-    popupLink.addEventListener("click", function (e) {
-      const popupName = popupLink.getAttribute("href").replace("#", " ");
-      const currentPopup = document.getElementById(popupName);
-      popupOpen(currentPopup);
-      e.preventDefault();
-    });
+  close() {
+      if (this.modalContainer) {
+          this.modalContainer.classList.remove('animate-open');
+          this.modalContainer.classList.remove(this.animation);
+          this.modal.classList.remove('is-open');
+          this.modalContainer.classList.remove('modal-open');
+
+          this.enableScroll();
+          this.options.isClose(this);
+          this.isOpen = false;
+          this.focusTrap();
+
+      }
+  }
+
+  focusCatch(e) {
+      const focusable = this.modalContainer.querySelectorAll(this.focusElements);
+  const focusArray = Array.prototype.slice.call(focusable);
+  const focusedIndex = focusArray.indexOf(document.activeElement);
+
+  if (e.shiftKey && focusedIndex === 0) {
+    focusArray[focusArray.length - 1].focus();
+    e.preventDefault();
+  }
+
+  if (!e.shiftKey && focusedIndex === focusArray.length - 1) {
+    focusArray[0].focus();
+    e.preventDefault();
+  }
+  }
+
+  focusTrap() {
+      const focusable = this.modalContainer.querySelectorAll(this.focusElements);
+      if (this.isOpen) {
+          focusable[0].focus();
+      } else {
+          this.previousActiveElement.focus();
+      }
+
+  }
+
+  disableScroll() {
+      let pagePosition = window.scrollY;
+  this.lockPadding();
+  document.body.classList.add('disable-scroll');
+  document.body.dataset.position = pagePosition;
+  document.body.style.top = -pagePosition + 'px';
+  }
+
+  enableScroll() {
+      let pagePosition = parseInt(document.body.dataset.position, 10);
+  this.unlockPadding();
+  document.body.style.top = 'auto';
+  document.body.classList.remove('disable-scroll');
+  window.scroll({ top: pagePosition, left: 0 });
+  document.body.removeAttribute('data-position');
+  }
+
+  lockPadding() {
+      let paddingOffset = window.innerWidth - document.body.offsetWidth + 'px';
+  this.fixBlocks.forEach((el) => {
+    el.style.paddingRight = paddingOffset;
+  });
+  document.body.style.paddingRight = paddingOffset;
+  }
+
+  unlockPadding () {
+      this.fixBlocks.forEach((el) => {
+    el.style.paddingRight = '0px';
+  });
+  document.body.style.paddingRight = '0px';
   }
 }
+
+const modal = new Modal({
+  isOpen: (modal) => {
+      console.log(modal);
+      console.log('opened');
+  },
+  isClose: () => {
+      console.log('closed');
+  },
+});
+
+
 
