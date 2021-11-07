2618f29f6b002099c491bc93335c633a92106840
romanshliakhov <romanshliakhov@gmail.com>
2021-11-07 16:52:55 +0200

    added input mask

--- source/js/common.js
+++ source/js/common.js
@@ -288,9 +288,9 @@ calculatorElements.forEach(calculatorElemnt => {
 
 
 // inputMask
-// let inputs = document.querySelectorAll('input[type="tel"]');
-// let im = new Inputmask('+7 (999) 999-99-99');
-// im.mask(inputs);
+let inputs = document.querySelectorAll('input[type="tel"]');
+let im = new Inputmask('+7 (999) 999-99-99');
+im.mask(inputs);
 
 // validate
 // function validateForms(selector, rules) {
