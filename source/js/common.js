adf36acc7a9e55fc5f6832fee0bfa857deb62c44
romanshliakhov <romanshliakhov@gmail.com>
2021-11-18 00:24:19 +0200

    added rename

--- source/js/common.js
+++ source/js/common.js
@@ -341,22 +341,6 @@ window.addEventListener("DOMContentLoaded", function () {
   });
 });
 
-// accordeon
-var acc = document.getElementsByClassName("accordion");
-var i;
-
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function () {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-    if (panel.style.maxHeight) {
-      panel.style.maxHeight = null;
-    } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
-    }
-  });
-}
-
 // menu dropdown
 (function () {
   'use strict';
